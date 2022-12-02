1e7359a0bd073265f701238142bde87cb4dd0236
mh02127 <majd.hafiri@stud.th-deg.de>
2022-12-02 14:53:42 +0100

    add post, update and delete requests

--- people.py
+++ people.py
@@ -1,4 +1,5 @@
 from datetime import datetime
+from flask import abort
 
 def get_timestamp():
     '''
@@ -26,3 +27,39 @@ PEOPLE = {
 
 def read_all():
     return list(PEOPLE.values())
+
+def create(person):
+    lname = person.get("lname")
+    fname = person.get("fname")
+
+    if (lname and lname) not in PEOPLE:
+        PEOPLE[lname] = {
+            "lname": lname,
+            "fname": fname,
+            "timestamp": get_timestamp(),
+        }
+        return PEOPLE[lname], 201
+    else:
+        abort(
+            406,
+            f"Person with last name {lname} already exists",
+        )
+
+def read_one(fname):
+    if fname in PEOPLE:
+        return PEOPLE[fname]
+
+    abort(404, f"person with last name {fname} not found!")
+
+def update(fname, person):
+    if fname in PEOPLE:
+        PEOPLE[fname]["lname"] = person.get("lname")
+        PEOPLE[fname]["timestamp"] = get_timestamp()
+        return PEOPLE[fname], 202
+    abort(404, f"person {fname} not found in dict")
+
+def delete(fname):
+    if fname in PEOPLE:
+        del PEOPLE[fname]
+        return f"{fname} Deleted successfully", 200
+    abort(404, f" {fname} Not found!")
